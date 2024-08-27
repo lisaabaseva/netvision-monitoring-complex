e22d164744d34784d0a7c21c41218e2d87155c5c
gwymlas <artem.kurakin.2013@mail.ru>
2024-08-27 14:07:39 +0300

    Добавление фронта в docker-compose

--- frontend/http/http.tsx
+++ frontend/http/http.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
+const BASE_URL = process.env.CENSUS_URL || 'http://localhost:8000/'
 
 
 const $host = axios.create({
-    baseURL: 'http://localhost:8000/'
+    baseURL: BASE_URL
 })
 
 export {
