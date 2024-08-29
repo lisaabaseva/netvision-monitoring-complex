41183b104b431984389711c1ac57ded16f98cfc2
gwymlas <artem.kurakin.2013@mail.ru>
2024-08-29 13:08:55 +0300

    census: Изменение подключения к БД (через URL.create )

--- census/app/config/init_db.py
+++ census/app/config/init_db.py
@@ -1,11 +1,25 @@
 from sqlmodel import create_engine, SQLModel, Session
+from sqlalchemy.engine import URL
+import logging
 
 from model import BaseClass
 from config import Config
 
 config = Config()
 
-engine = create_engine(config.DATABASE_URL, echo=True)
+logger = logging.getLogger('census logs')
+logger.warning(f'config.DATABASE_CONFIG.DRIVERNAME: {config.DATABASE_CONFIG.DRIVERNAME}')
+
+url_object = URL.create(
+    drivername=config.DATABASE_CONFIG.DRIVERNAME,
+    port=config.DATABASE_CONFIG.DB_PORT,
+    username=config.DATABASE_CONFIG.DB_USERNAME,
+    password=config.DATABASE_CONFIG.DB_PASSWORD,
+    host=config.DATABASE_CONFIG.DB_HOST,
+    database=config.DATABASE_CONFIG.DB_NAME,
+    query={'application_name': config.DATABASE_CONFIG.APPLICATION_NAME}
+)
+engine = create_engine(url_object, echo=True)
 BaseClass.metadata.create_all(engine)
 
 
